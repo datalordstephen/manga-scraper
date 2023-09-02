5d74217238d7afad0f94f2e991211e1a08d9c4c5
Stephen Adegbokun <obaadegbokun1@gmail.com>
2023-09-02 05:35:52 +0100

    feat: gotten a working tool

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-import requests
 import os
+from tqdm import tqdm
 import click
 import shutil
 from src.utils import get_image_url, download_page, get_number_of_chapters
-import sys
 
 BASE_URL = "https://www.mangasee123.com/manga/"
 BASE_IMG_URL = "https://hot.leanbox.us/manga/"
@@ -13,24 +12,33 @@ BASE_IMG_URL = "https://hot.leanbox.us/manga/"
 @click.command()
 @click.option('--name', default="vinland saga", help = "Name of manga you want to download (default: Vinland saga)")
 @click.option('--start', default = 1, help = "First chapter to download")
-@click.option('--last', default = 10, help = "Last chapter to download")
-@click.option('--save_dir', default = os.path.join(os.getcwd(), "manga"), help="Path to save the manga")
+@click.option('--end', default = 10, help = "Last chapter to download")
+@click.option('--save_dir', default = os.path.join(os.getcwd(), "manga"),
+              help="Absolute path to save the manga. If not specified, a dir `manga` will be created in the pwd and \
+                  the manga will be saved in a sub folder with the name of the manga e.g manga/vinland-saga")
 
-def main(name, start, last, save_dir) -> None:
-    no_chapters = get_number_of_chapters(BASE_URL, name)
-    sys.exit(0)
+def main(name, start, end, save_dir) -> None:
+    no_chapters, formatted_name = get_number_of_chapters(BASE_URL, name)
+
+    save_dir = os.path.join(save_dir, formatted_name)
     
+    if end > no_chapters:
+        print(f"{formatted_name} only has {no_chapters} chapters, {end} exceeds that")
+        raise ValueError("Last chapter exceeds the number of chapters in the manga")
     
     if os.path.exists(save_dir):
         print(f"Path: {save_dir} already exists, deleting it...")
         shutil.rmtree(save_dir)
+        
     os.makedirs(save_dir)
     print("Created: {}".format(save_dir))
+    print("-" *50, end="\n\n")
         
-    for chapter_num in range(start, last + 1):
+    print(f"Dowloading chapters {start} to {end} of {formatted_name}".title())
+    
+    for chapter_num in tqdm(range(start, end + 1), desc="Chapters Downloaded", ncols=100):
         chapter_path = os.path.join(save_dir, f"chap-{chapter_num}")
         os.mkdir(chapter_path)
-        print(f"Created the chapter {chapter_num} directory. Downloading pages...")
         page_num = 1
         
         while True:
@@ -42,6 +50,7 @@ def main(name, start, last, save_dir) -> None:
                 page_num += 1
             else:
                 break
+    print("\nDownload Completed!")
         
             
         
--- src/utils.py
+++ src/utils.py
@@ -20,15 +20,14 @@ def get_number_of_chapters(base_url: str, name: str) -> int:
     formatted_str = name.title().replace(" ", "-")
     
     right_name = convert_anime_name(formatted_str)
-    right_name = name.title().replace(" ", "-")
+    right_name = right_name.title().replace(" ", "-")
     
     if right_name != formatted_str:
-        print(f"By {formatted_str} did you mean {right_name}?")
+        print(f"By {name} did you mean {right_name}?")
         formatted_str = right_name
         
     url = urljoin(base_url, formatted_str)
     print("Getting number of chapters of {}...".format(formatted_str))
-    print(url)
     
     service = Service(executable_path=DRIVER_PATH)
     options = ChromeOptions()
@@ -51,8 +50,9 @@ def get_number_of_chapters(base_url: str, name: str) -> int:
     latest_chapter = latest_chapter_span.text.strip()
     chapter = latest_chapter.split()[1]
     print(f"{formatted_str} currently ends at chapter {chapter}")
+    print("-" * 50)
     
-    return int(chapter)
+    return int(chapter), formatted_str
 
     
 
@@ -65,7 +65,10 @@ def get_image_url(base_url: str, name: str, chapter: int, page: int) -> str:
     page_zeros = (3 - len(str(page))) * "0"
     _page = f"{page_zeros}{page}"
     
-    search_url = urljoin(base_url, formatted_str, f"{_chapter}-{_page}.png")
+    joined = f"{_chapter}-{_page}.png"
+    sub_url = formatted_str + "/" + joined
+    
+    search_url = urljoin(base_url, sub_url)
     return search_url
 
 
@@ -74,15 +77,14 @@ def download_page(img_url: str, output_path: str, chapter_path: str, page_num: i
     
     res = requests.get(img_url)
     
-    status = False
+    not_found = False
     if res.status_code == 200:
         with open(output_path, "wb") as file:
             file.write(res.content)
-        print(f"Page {page_num} downloaded.")
     elif res.status_code == 404:
-        status = True
+        not_found = True
     
-    return status
+    return not_found
         
     
 if __name__ == "__main__":
